de3bf522e6a2ca1dacfdf7a99f662924efa793b4
Jennifer Rhees <redswinglinestapler23@gmail.com>
2017-11-26 23:06:20 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -6,16 +6,6 @@ const SiteController = require("./controllers/SiteController")
 app.get("/", SiteController.Index)
 app.get("/about", SiteController.About)
 app.get("/contact", SiteController.Contact)
-  
+
 
 module.exports = app
-
-
-
-
-
-
-
-
-
-
